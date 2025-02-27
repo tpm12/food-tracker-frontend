363a3bfef5d49b9dd2ab65844c1211ba89d15585
Oliver <OliverBalthasar.Schmidt@student.hs-anhalt.de>
2025-02-27 22:57:00 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Food Tracker</h1>
+      <p>Track your calories and water intake easily!</p>
+    </div>
+  );
+};
+
+export default App;
